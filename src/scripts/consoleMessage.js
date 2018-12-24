f83d89be6d72bec9cd1468236618367d69f5c324
Oleg <zizu19988@gmail.com>
2018-12-24 21:38:57 +0200

    (feature/console-message) Add styles to message

--- src/scripts/consoleMessage.js
+++ src/scripts/consoleMessage.js
@@ -1,3 +1,10 @@
 export default function consoleMessage() {
-    console.log('Coded with ♥️ by Oleg Savenok');
+    const consoleMessageStyle = [
+        'background: #fff',
+        'color: #000',
+        'padding: 10px 20px',
+        'line-height: 35px',
+        'border: 2px solid #000',
+    ].join(';');
+    console.log('%c Coded with ♥️ by Oleg Savenok', consoleMessageStyle);
 }
