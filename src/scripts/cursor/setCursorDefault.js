ef024744320fa4122adc08215015b66c4789cc94
Oleg <zizu19988@gmail.com>
2018-12-26 16:09:46 +0200

    (feature/cursor) Refactor setCursorDefault module

--- src/scripts/cursor/setCursorDefault.js
+++ src/scripts/cursor/setCursorDefault.js
@@ -6,12 +6,13 @@ export default function setCursorDefault() {
     let {
         cursorIcon,
         size: { basic: sizeBasic },
+        opacity: { basicMode: opacityBasicMode },
     } = cursorOptions;
 
     TweenMax.to(cursor, 0.2, {
         height: sizeBasic,
         width: sizeBasic,
-        opacity: 0,
+        opacity: opacityBasicMode,
     });
 
     TweenMax.to(cursorIcon, 0.1, { opacity: 0 });
