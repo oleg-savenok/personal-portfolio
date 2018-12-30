b15db9bd5bd1fc8b376d15008d45cede13ca5b3d
Oleg <zizu19988@gmail.com>
2018-12-30 02:42:07 +0200

    (feature/cursor) Add new callbacks to routes

--- src/scripts/routesCallback.js
+++ src/scripts/routesCallback.js
@@ -1,19 +1,23 @@
-import cursor from './cursor/cursor';
+import Cursor from './cursor/cursor';
 import projectsSlider from './projectsSlider';
 
 export default function routerCallback(data) {
+    const cursor = new Cursor();
+
     // Define callbacks for all pages
     const callbacks = {
         home: () => {
             projectsSlider();
+            cursor.addProjectsListeners();
         },
-        about: () => {},
-        contact: () => {},
+        about: () => {
+            cursor.removeProjectsListeners();
+        },
+        contact: () => {
+            cursor.removeProjectsListeners();
+        },
     };
 
     // Call callback functions
     callbacks[data.name]();
-
-    // Reload cursor
-    cursor();
 }
