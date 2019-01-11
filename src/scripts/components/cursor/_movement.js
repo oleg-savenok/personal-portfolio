72fc565cce94529f742185466d132720108b050e
Oleg <zizu19988@gmail.com>
2019-01-11 10:55:37 +0200

    (feature/cursor-fixing) Fixed show/hide actions

--- src/scripts/components/cursor/_movement.js
+++ src/scripts/components/cursor/_movement.js
@@ -10,28 +10,22 @@ export default class Movement {
         this.scrollTop = scrollTop;
         this.position = position;
         this.tickDuration = tickDuration;
-        this.tickTweenDuration = 0;
         this.cursorHide = true;
     }
 
-    getRealMousePosition(e) {
+    moveAnimation(e) {
         this.position.x = e.pageX;
         this.position.y = e.pageY - this.scrollTop;
-    }
 
-    moveAnimation(duration) {
-        TweenMax.to(this.cursor, duration, {
+        TweenMax.to(this.cursor, this.cursorHide ? 0 : this.tickDuration, {
             x: this.position.x,
             y: this.position.y,
         });
     }
 
     showCursor() {
-        TweenMax.to(this.cursor, 0.1, {
+        TweenMax.to(this.cursor, 0.05, {
             alpha: 1,
-            onComplete: () => {
-                this.tickTweenDuration = this.tickDuration;
-            },
         });
 
         this.cursorHide = false;
@@ -40,19 +34,18 @@ export default class Movement {
     hideCursor() {
         TweenMax.set(this.cursor, { alpha: 0 });
 
-        this.tickTweenDuration = 0;
         this.cursorHide = true;
     }
 
     init() {
         // Set ticker listener for change position of magic cursor
-        TweenMax.ticker.addEventListener('tick', () => {
-            this.moveAnimation(this.tickTweenDuration);
-        });
+        // TweenMax.ticker.addEventListener('tick', () => {
+        //     this.moveAnimation(this.tickTweenDuration);
+        // });
 
         // If real cursor moving - change position of magic cursor
         document.addEventListener('mousemove', (e) => {
-            this.getRealMousePosition(e);
+            this.moveAnimation(e);
 
             // Show magic cursor if hidden
             if (this.cursorHide) {
@@ -61,9 +54,9 @@ export default class Movement {
         });
 
         // If cursor enter document - show
-        document.addEventListener('mouseenter', () => {
-            this.showCursor();
-        });
+        // document.addEventListener('mouseenter', () => {
+        //     this.showCursor();
+        // });
 
         // If cursor leave document - hide
         document.addEventListener('mouseleave', () => {
