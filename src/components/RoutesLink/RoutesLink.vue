09eab15b2df46d47d7d20b2e74a0ded545ce835c
Oleg <zizu19988@gmail.com>
2019-05-11 15:32:01 +0300

    (vue-feature/active-router-link) Add RouterLink component and animation for opened link

---
+++ src/components/RoutesLink/RoutesLink.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="router-link">
+		<router-link :to="link">{{ text }}</router-link>
+		<router-link to="/">
+			<span>c</span>
+			<span>l</span>
+			<span>o</span>
+			<span>s</span>
+			<span>e</span>
+		</router-link>
+	</div>
+</template>
+
+<script>
+    export default {
+        name: 'RoutesLink',
+	    props: {
+            text: String,
+		    link: String
+	    },
+	    methods: {
+            historyBack: () => {
+                history.back();
+            }
+	    }
+    };
+</script>
+
+<!-- Styles -->
+<style scoped lang="scss" src="./RoutesLink.scss"></style>
