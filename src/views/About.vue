ca9497c0c7fcacba736d6a458820378eaa14728f
Oleg <zizu19988@gmail.com>
2019-05-11 22:03:16 +0300

    (vue-feature/smooth-scroll) Add smooth scroll to About and Contact pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="page">
-        <h3>About page</h3>
+	    <SmoothScroll>
+		    <br>
+		    <br>
+		    <h3>About page</h3>
+		    <br>
+		    <img src="https://images.pexels.com/photos/2067638/pexels-photo-2067638.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+		    <br>
+		    <br>
+		    <br>
+		    <br>
+	    </SmoothScroll>
     </div>
 </template>
+
+<script>
+	import SmoothScroll from '@/components/SmoothScroll/SmoothScroll';
+	
+	export default {
+	    name: 'About',
+		components: {
+	        SmoothScroll
+        }
+	}
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="page">
-        <h3>Contact page</h3>
+	    <SmoothScroll>
+		    <br>
+		    <br>
+		    <h3>Contact page</h3>
+		    <br>
+		    <img src="https://images.pexels.com/photos/2067468/pexels-photo-2067468.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+		    <br>
+		    <br>
+		    <br>
+		    <br>
+	    </SmoothScroll>
     </div>
 </template>
+
+<script>
+    import SmoothScroll from '@/components/SmoothScroll/SmoothScroll';
+    
+    export default {
+        name: 'Contact',
+        components: {
+            SmoothScroll
+        }
+    }
+</script>
